418502644106b223258c17cbf8c91a2440af58fb
AlexHtZhang <alexander.ht.zhang@gmail.com>
2018-08-06 02:58:52 -0700

    system I/O done

--- notes.js
+++ notes.js
@@ -9,8 +9,24 @@ var addNote = (title, body) => {
 		body
 	};
 
-	notes.push(note);
-	fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+	try {
+	  	var notesString = fs.readFileSync('notes-data.json');
+		notes = JSON.parse(notesString);
+	} catch (e) {
+		// don't have to wirte anythig here. the error will be catched and the file will be created.
+	}
+
+	// arror function no need for return. it will auto return the expresion.
+	var duplicateNotes = notes.filter((note) => note.title === title);
+	// note.title is the title of all notes in the notes one by one.
+	// filter method return an array of elements survive the callback function.
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+
+	if (duplicateNotes.length === 0) {
+		notes.push(note);
+		fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+	}
+
 };
 
 var getAll = () => {
